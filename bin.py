4d2c82fcaec6d61d14e077ee3e538ca96371e8f8
Grigory <gtsyrfa@icore.local>
2019-06-28 10:44:03 +0300

    Update some packages; Add class barbarian

--- bin.py
+++ bin.py
@@ -1,17 +1,20 @@
-class cl_test:
-    def _notsoprivate(self):
-        print("Not so private")
-    
-    def __soprivate(self):
-        print("More private text")
+from testmodule import warrior
+from testmodule import weapon
+from testmodule import barbarian
 
 
-
-tmp_var = cl_test()
-
-
-tmp_var._notsoprivate()
+sword = weapon(10,5,5,"sword")
+sword_and_sheld=weapon(12,5,12,"sword_and_sheld")
+axe=weapon(12,12,5,"axe")
 
 
-tmp_var._cl_test__soprivate()
-tmp_var._cl_test_notsoprivate()
+if __name__ == "__main__":
+    peter = barbarian(name = "Peter",weapon = axe)
+    bob = warrior(200,5,"Bob",weapon = sword)
+    #print(peter.__dict__) # print all varriavles object peter
+    (peter.greet())
+    (bob.greet())
+    #peter.attack(bob)
+    #print(isinstance(peter,warrior))
+    #type(peter)
+    peter.fight(bob)
--- testmodule.py
+++ testmodule.py
@@ -1,27 +1,74 @@
+from random import randint
+
 class weapon:
-    def __init__(self, damage=3):
-        self.dmg=damage
+    def __init__(self, damage=3,attack=3,defence=3,name="hands"):
+        self.damage = damage
+        self.attack = attack
+        self.defence = defence
+        self.name = name
 
 
 
 
+
+HAND = weapon()
 
 class warrior:
-    def __init__(self,hp=100,damage=10,name="noName"):
+    def __init__(self,hp=100,strong=3,name="noName",weapon=HAND):
         self.name = name
         self.currenthp = hp
-        self.basedamage = damage
+        self.strong = strong
+        self.weapon = weapon
+        self.sumdmg = self.strong + self.weapon.damage
 
 
-    #weapon weapinhand = hand
-    #int BASEATTACK = 5
-    #int BASEDEFFENCE = 10
+
+
+
     def attack(self,target):
-        print(f"{self.name} attacked {target.name} witch {self.basedamage}")
-        target.currenthp = target.currenthp - self.basedamage #+ weapon.dmg
-        print(f"{target.name} after attack have {target.currenthp}")
-    #def get-damage(self,attacker):
-    def meeting(self):
-        return(f"is fighter with {self.currenthp} HP and {self.basedamage} damage")
+        if isinstance(target,warrior):
+            sumattack=self.weapon.attack+self.strong+randint(1,20)
+            print(f"{self.name} try to attack {target.name} "\
+                  f"witch attack {sumattack}")
+            summdef = target.weapon.defence+target.strong+randint(1,20)
+            print(f"{target.name} try to defence {summdef}")
+            if (sumattack-summdef>0):
+                __dmg=randint((self.sumdmg//2),self.sumdmg)
+                target.currenthp -=  __dmg
+                print(f"Attack is successful. Damage is { __dmg}")
+            else:
+                print("Miss!")
+            print(f"{target.name} after attack have {target.currenthp} HP")
+            return(sumattack-summdef)
+        else:
+            print("only warriors can attack each other")
+            return 0
+        
 
 
+ 
+    def greet(self):
+        print(f"{self.name} is fighter with {self.currenthp} HP "\
+              f"and {self.strong} basedamage. He weaponed is "\
+              f"{self.weapon.name} witch damage {self.weapon.damage}")
+    
+    
+    
+    def fight(self,target):
+        if self.currenthp<0:
+            print(f"{target.name} WIN!")
+        else:
+                type(self).attack(self,target)
+                type(self).fight(target,self)
+
+
+
+
+class barbarian(warrior):
+    def attack(self,target):
+        __attackdifference = (warrior.attack(self,target))
+        #print("Debug>>>",__attackdifference)
+        #print("Debug>>>",type(__attackdifference))
+        if __attackdifference>5:
+            print("Second attack witch FURRY!")
+            warrior.attack(self,target)
